fd9921b1facee9101bb1c5cb83a0cc23a954d883
Vansama Wang <185226903@qq.com>
2022-06-20 00:44:43 +0800

    c

---
+++ consumer/src/test/java/org/vasa/consumer/JunitTest.java
@@ -0,0 +1,10 @@
+package org.vasa.consumer;
+
+import org.junit.jupiter.api.Test;
+
+public class JunitTest {
+    @Test
+    public void junit_test(){
+        System.out.println("J U N I T..............");
+    }
+}
---
+++ consumer/src/test/java/org/vasa/consumer/SmokeTest.java
@@ -0,0 +1,7 @@
+package org.vasa.consumer;
+
+public class SmokeTest {
+    public void test_smoke(){
+        System.out.println("V A N S A M A .....................");
+    }
+}
